8735bcdf3ec692642a1ec9222a5d2c5756d1893d
kiryuhakipyatok <goodapple5207@gmail.com>
2025-02-16 00:16:39 +0300

    update_main

--- internal/server/server.go
+++ internal/server/server.go
@@ -5,18 +5,19 @@ import (
 	"log"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"os"
 )
 func RunServer() *fiber.App{
     app:=fiber.New()    
 	app.Static("pkg","../../pkg")
     app.Use(cors.New(cors.Config{        
-		AllowOrigins:     "http://localhost:3000,http://localhost:9110",
+		AllowOrigins:     "*",
         AllowMethods:     "GET,POST,PUT,DELETE,OPTIONS,PATCH",         
 		AllowHeaders:     "Origin,Content-Type,Accept,Authorization", 
         ExposeHeaders:    "Content-Length",        
-		AllowCredentials: true, 
+		AllowCredentials: false, 
     }))    
 	routes.Setup(app)
-    log.Fatal(app.Listen(":9110"))    
+    log.Fatal(app.Listen(os.Getenv("PORT")))    
 	return app
 }
