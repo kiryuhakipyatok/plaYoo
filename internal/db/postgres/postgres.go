61ffb3b07b53ab11ea0b29f65e96cccfe9d688e2
kiryuhakipyatok <goodapple5207@gmail.com>
2025-02-27 20:21:03 +0300

    forserver_feature

--- internal/db/postgres/postgres.go
+++ internal/db/postgres/postgres.go
@@ -5,29 +5,29 @@ import (
 	
 	"log"
 	
-
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"os"
 )
 
 var Database *gorm.DB
 
-
 func ConnectToPostgres() error{
-	// if err := godotenv.Load();err != nil {
-    //     log.Fatal("Error loading .env file"+ err.Error())
-    // }
+	if err := godotenv.Load();err != nil {
+        log.Fatal("Error loading .env file"+ err.Error())
+    }
 	
-	// var (
-	// 	database   = os.Getenv("DB_DATABASE_RLW")
-	// 	password   = os.Getenv("DB_PASSWORD_RLW")
-	// 	username   = os.Getenv("DB_USERNAME_RLW")
-	// 	port       = os.Getenv("DB_PORT_RLW")
-	// 	host       = os.Getenv("DB_HOST_RLW")
-	// )
-	// if database == "" || username == "" || password == "" || host == "" || port == "" {
-    //     log.Fatal("One or more environment variables are not set")
-    // }
+	var (
+		database   = os.Getenv("DB_DATABASE_RLW")
+		password   = os.Getenv("DB_PASSWORD_RLW")
+		username   = os.Getenv("DB_USERNAME_RLW")
+		port       = os.Getenv("DB_PORT_RLW")
+		host       = os.Getenv("DB_HOST_RLW")
+	)
+	if database == "" || username == "" || password == "" || host == "" || port == "" {
+        log.Fatal("One or more environment variables are not set")
+    }
 
 	dsn:="host=junction.proxy.rlwy.net user=postgres password=ZFvBKMDypzRLBbFqMHMOiRCmFvMPPLCv dbname=railway port=43543 sslmode=disable TimeZone=Europe/Minsk"
 	connection,err:= gorm.Open(postgres.Open(dsn),&gorm.Config{})
