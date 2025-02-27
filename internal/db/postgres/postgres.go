799301e6e5d7506621d7819e3445211b2616e958
kiryuhakipyatok <goodapple5207@gmail.com>
2025-02-27 19:51:51 +0300

    forserver_feature

--- internal/db/postgres/postgres.go
+++ internal/db/postgres/postgres.go
@@ -12,7 +12,12 @@ import (
 
 var Database *gorm.DB
 
-func ConnectToPostgres(){
+
+func ConnectToPostgres() error{
+	// if err := godotenv.Load();err != nil {
+    //     log.Fatal("Error loading .env file"+ err.Error())
+    // }
+	
 	// var (
 	// 	database   = os.Getenv("DB_DATABASE_RLW")
 	// 	password   = os.Getenv("DB_PASSWORD_RLW")
@@ -20,16 +25,24 @@ func ConnectToPostgres(){
 	// 	port       = os.Getenv("DB_PORT_RLW")
 	// 	host       = os.Getenv("DB_HOST_RLW")
 	// )
+	// if database == "" || username == "" || password == "" || host == "" || port == "" {
+    //     log.Fatal("One or more environment variables are not set")
+    // }
+
 	dsn:="host=junction.proxy.rlwy.net user=postgres password=ZFvBKMDypzRLBbFqMHMOiRCmFvMPPLCv dbname=railway port=43543 sslmode=disable TimeZone=Europe/Minsk"
 	connection,err:= gorm.Open(postgres.Open(dsn),&gorm.Config{})
 	if err!=nil{
-		log.Printf("Error connect to database: %v", err)
+		return err
 	}
+	
 	Database = connection
-	connection.AutoMigrate(&models.User{})
-	connection.AutoMigrate(&models.Event{})
-	connection.AutoMigrate(&models.Comment{})
-	connection.AutoMigrate(&models.News{})
-	connection.AutoMigrate(&models.Game{})
-	connection.AutoMigrate(&models.Notice{})
+	connection.AutoMigrate(
+		&models.User{},
+		&models.Event{},
+		&models.Comment{},
+		&models.News{},
+		&models.Game{},
+		&models.Notice{})
+	log.Printf("Connect to postgress successfully")
+	return nil
 }	
