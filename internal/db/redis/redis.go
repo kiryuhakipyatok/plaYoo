d96574e20505c87a684cf920bf192430e6dd9191
kiryuhakipyatok <goodapple5207@gmail.com>
2025-02-27 19:54:41 +0300

    forserver_feature

--- internal/db/redis/redis.go
+++ internal/db/redis/redis.go
@@ -2,6 +2,7 @@ package redis
 
 import (
 	"github.com/redis/go-redis/v9"
+	"log"
 	"context"
 )
 
@@ -10,11 +11,19 @@ var (
 	Ctx context.Context
 )
 
-func ConnectToRedis(){
+func ConnectToRedis() error{
 	client := redis.NewClient(&redis.Options{
-        Addr:     "localhost:6666",           
+        Addr:     "localhost:6379",
+        Password: "",               
         DB:       0,              
     })
+	// defer client.Close()
 	Rdb = client
 	Ctx = context.Background()
+	if _,err:= client.Ping(Ctx).Result();err!=nil{
+		return err
+	}else{
+		log.Printf("Connect to redis successfully")
+	}
+	return nil
 }
