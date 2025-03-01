a24e0174f2e9325050878de75f12512fdf993b69
kiryuhakipyatok <goodapple5207@gmail.com>
2025-03-01 16:07:45 +0300

    type_fix

--- internal/models/notice.go
+++ internal/models/notice.go
@@ -4,6 +4,6 @@ import "github.com/google/uuid"
 
 type Notice struct {
 	Id      uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primary_key" json:"notice_id"`
-	EventId uuid.UUID `gorm:"type:uuid[];not null"`
+	EventId uuid.UUID `gorm:"type:uuid;not null"`
 	Body    string    `json:"body" gorm:"not null"`
 }
